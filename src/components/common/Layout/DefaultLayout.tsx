6c012089cd28a77b0970ae51b368320785050b70
mie6996 <nguyenxuanphat060301@gmail.com>
2023-01-02 17:31:01 +0700

    fix toast notify payment invoice

--- src/components/common/Layout/DefaultLayout.tsx
+++ src/components/common/Layout/DefaultLayout.tsx
@@ -31,37 +31,6 @@ function Layout({ children }: Props) {
   useEffect(() => {
     if (alertMessage) {
       toastNotify(alertMessage);
-      // toast.custom((t) => (
-      //   <div
-      //     className={`${
-      //       t.visible ? "animate-enter" : "animate-leave"
-      //     } pointer-events-auto flex w-full max-w-md rounded-lg bg-white shadow-lg ring-1 ring-black ring-opacity-5`}
-      //   >
-      //     <div className="w-0 flex-1 p-4">
-      //       <div className="flex items-start">
-      //         <div className="flex-shrink-0 pt-0.5">
-      //           <TfiClose className="h-6 w-6 text-red-400" />
-      //         </div>
-      //         <div className="ml-3 flex-1">
-      //           <p className="text-sm font-medium text-gray-900">
-      //             Emilia Gates
-      //           </p>
-      //           <p className="mt-1 text-sm text-gray-500">
-      //             Sure! 8:30pm works great!
-      //           </p>
-      //         </div>
-      //       </div>
-      //     </div>
-      //     <div className="flex border-l border-gray-200">
-      //       <button
-      //         onClick={() => toast.dismiss(t.id)}
-      //         className="flex w-full items-center justify-center rounded-none rounded-r-lg border border-transparent p-4 text-sm font-medium text-indigo-600 hover:text-indigo-500 focus:outline-none focus:ring-2 focus:ring-indigo-500"
-      //       >
-      //         Close
-      //       </button>
-      //     </div>
-      //   </div>
-      // ));
       setAlertMessage(null);
     }
   }, [alertMessage]);
