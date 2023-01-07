e725869a41d865a970187582a3503b6df2c93f3f
Tin Nguyen <hopthucuatin@gmail.com>
2023-01-07 11:37:15 +0700

    style transaction

--- src/components/user/transaction/TransactionsManager.tsx
+++ src/components/user/transaction/TransactionsManager.tsx
@@ -2,6 +2,7 @@ import Card from "@/components/common/Card/Card";
 import Heading from "@/components/common/Heading/Heading";
 import Spinner from "@/components/common/Spinner/Spinner";
 import { useQueryGetTransactions } from "@/lib/home/hooks/transaction/useQueryGetTransactions";
+import moment from "moment";
 import React from "react";
 
 function TransactionsManager() {
@@ -32,8 +33,6 @@ function TransactionsManager() {
     offset: 0,
   });
 
-  console.log(transactionsQuery.data);
-
   return (
     <>
       <Card>
@@ -62,18 +61,47 @@ function TransactionsManager() {
         ) : (
           // eslint-disable-next-line @typescript-eslint/ban-ts-comment
           // @ts-ignore
-          transactionsQuery.data?.data.map((transaction: any) => {
-            return (
-              <div key={transaction.id}>
-                <div>{transaction.id}</div>
-                <div>{transaction.amount}</div>
-                <div>{transaction.message}</div>
-                <div>{transaction.type}</div>
-                <div>{transaction.fromCustomer.accountNumber}</div>
-                <div>{transaction.createdAt}</div>
-              </div>
-            );
-          })
+          <div className="grid gap-2 md:grid-cols-2">
+            {(transactionsQuery.data as any)?.data.map((transaction: any) => {
+              return (
+                <div
+                  key={transaction.id}
+                  className="flex items-center rounded-md bg-white p-4 shadow"
+                >
+                  <div>
+                    <div className="w-32 text-2xl tracking-wider text-green-500">
+                      +{Intl.NumberFormat("en-US").format(transaction.amount)}$
+                    </div>
+                  </div>
+                  <div className="grow">
+                    <div className="text-xs text-gray-700">
+                      <div className="text-lg font-light">
+                        {transaction.fromCustomer.firstName}{" "}
+                        {transaction.fromCustomer.lastName}
+                        {" - "}
+                        {transaction.fromCustomer.accountNumber}
+                      </div>
+                      <div>
+                        <b>Message:</b>
+                        <p>{transaction.message}</p>
+                      </div>
+                      <div className="my-2"></div>
+                      <div className="text-gray-500">
+                        {transaction.type === "INTERNAL"
+                          ? "Cùng ngân hàng"
+                          : "Liên ngân hàng"}{" "}
+                        -{" "}
+                        {moment(transaction.createdAt).format(
+                          "HH:mm:ss DD/MM/YYYY"
+                        )}
+                        <div>#{transaction.id}</div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
         )}
       </Card>
     </>
@@ -81,3 +109,5 @@ function TransactionsManager() {
 }
 
 export default TransactionsManager;
+
+TransactionsManager.title = "Transactions";
--- src/pages/user/transactions/index.tsx
+++ src/pages/user/transactions/index.tsx
@@ -1,10 +1,3 @@
 import TransactionsManager from "@/components/user/transaction/TransactionsManager";
-import React from "react";
 
-function Index() {
-  return <TransactionsManager />;
-}
-
-Index.title = "Transactions";
-
-export default Index;
+export default TransactionsManager;
